f39fbf77e93f0da9ab7d3cd16f4f362ffecad875
sabbiu <070bct531@ioe.edu.np>
2018-12-21 23:16:07 +0545

    netlify fixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -81,6 +81,12 @@ module.exports = {
         pathToConfigModule: 'src/utils/typography',
       },
     },
-    `gatsby-plugin-netlify-cms`,
+    {
+      resolve: 'gatsby-plugin-netlify-cms',
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
+    'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
 }
---
+++ src/cms/cms.js
@@ -0,0 +1,5 @@
+import CMS from 'netlify-cms'
+
+import BlogPostPreview from './preview-templates/BlogPostPreview'
+
+CMS.registerPreviewTemplate('blog', BlogPostPreview)
